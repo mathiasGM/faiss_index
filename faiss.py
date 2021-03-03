1a67180d774d3e6ef8d575c5a922f0662eb9f15c
Mathias Giovanni Møller <33517196+mathiasGM@users.noreply.github.com>
2021-03-03 20:56:34 +0100

    Update faiss.py

--- faiss.py
+++ faiss.py
@@ -1,10 +1,12 @@
 import faiss
 
+dimension = 3
+
 v_1 = np.array([[1,2,3]]).astype('float32')
 v_2 = np.array([[4,5,6]]).astype('float32')
 v_3 = np.array([[7,8,9]]).astype('float32')
 
-index = faiss.IndexIDMap2(faiss.IndexFlat(3))
+index = faiss.IndexIDMap2(faiss.IndexFlat(dimension))
 
 index.add_with_ids(v_1, np.array([10]))
 index.add_with_ids(v_2, np.array([20]))
