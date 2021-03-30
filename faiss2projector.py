ac6dc789896d61046746b44decfdf63e32bb7391
Mathias Giovanni Møller <33517196+mathiasGM@users.noreply.github.com>
2021-03-30 13:18:44 +0200

    Update faiss2projector.py

--- faiss2projector.py
+++ faiss2projector.py
@@ -1,6 +1,5 @@
-"""
-Take a Faiss index, retrieve its vectors and visualize them using tensorflow embedding projector
-"""
+# Helper functions for faiss2projector. 
+# Concept: Take a Faiss index, retrieve its vectors and visualize them using tensorflow embedding projector
 
 import os, faiss
 import numpy as np
@@ -8,19 +7,12 @@ import tensorflow as tf
 from tensorboard.plugins import projector
 
 
-### TO DO ###
-
-# 1) Go to notebook and add dictionary save with title 
-# 2) Read index, dictionary
-# 3) Make embeddings projector
-
 def load_faiss_index(path):
     if os.path.isfile(path):
         index = faiss.read_index(path)
         return index
     else:
         raise FileNotFoundError
-
 
 def retrieve_vectors_faiss(index, index_dict):
 
@@ -32,40 +24,26 @@ def retrieve_vectors_faiss(index, index_dict):
     return reconstructed_vectors
 
 
-def write_meta_data(index_dict, path):
+def write_meta_data(index_dict, log_dir):
 
     with open(os.path.join(log_dir, 'metadata.tsv'), "w") as f:
         for id_ in index_dict.values():
             f.write("{}\n".format(id_))
 
 
-# Directory log for tensorboard
-log_dir = 'logs'
+def set_checkpoint_and_config(embeddings, log_dir):
+    # Save embeddings to tensorflow board
+    embeddings = tf.Variable(embeddings)
 
-# Index
-index = load_faiss_index("index")
-print(index.ntotal)
+    checkpoint = tf.train.Checkpoint(embedding=embeddings)
+    checkpoint.save(os.path.join(log_dir, "embedding.ckpt"))
 
-ids = [10, 20, 30]
-ids_dict = {
-    10 : "Number 10",
-    20 : "Number 20",
-    30 : "Number 30",
-}
+    config = projector.ProjectorConfig()
+    embedding = config.embeddings.add()
 
-
-# Save embeddings to tensorflow board
-embeddings = tf.Variable(reconstructed_vectors)
-
-checkpoint = tf.train.Checkpoint(embedding=embeddings)
-checkpoint.save(os.path.join(log_dir, "embedding.ckpt"))
-
-config = projector.ProjectorConfig()
-embedding = config.embeddings.add()
-
-embedding.tensor_name = "embedding/.ATTRIBUTES/VARIABLE_VALUE"
-embedding.metadata_path = 'metadata.tsv'
-projector.visualize_embeddings(log_dir, config)
+    embedding.tensor_name = "embedding/.ATTRIBUTES/VARIABLE_VALUE"
+    embedding.metadata_path = 'metadata.tsv'
+    projector.visualize_embeddings(log_dir, config)
 
 # Run: 
 #tensorboard --logdir logs
